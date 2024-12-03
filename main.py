af47ff727883ebbffe8c972a4b93d9795ae6d497
meerimvz <meerimzulpukarova5@gmail.com>
2024-12-03 23:59:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -129,33 +129,12 @@ def mark_sunk(board, ship):
 def check_victory(ships, board):
     return all(all(board[r][c] == 'S' for r, c in ship) for ship in ships)
 
-
-    
-
-    if board[x][y] != " " and board[x][y] != HIT and board[x][y] != MISS:
-        board[x][y] = HIT
-        for ship in ships:
-            if ship[0] == (x, y):  
-                ship[2] -= 1  
-                if ship[2] == 0:  
-                    mark_sunk(board, ship)
-        return HIT
-    else:
-        board[x][y] = MISS
-        return MISS
-
-def mark_sunk(board, ship):
-    for i in range(ship[2]):
-        x, y = ship[0]
-        if ship[1] == "horizontal":
-            board[x][y + i] = SUNK
-        elif ship[1] == "vertical":
-            board[x + i][y] = SUNK
-
 def play_game():
     player_name = input("Enter your name: ")
-    board, ships = place_ships()
-    shots_taken = 0
+    clear_screen()
+    player_board = create_empty_board()
+    ships = field_generation()
+    shots = 0
     
 while True:
         display_board(board)
